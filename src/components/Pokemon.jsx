05b8f9ddd79126cc639b5351fd944a2b8f17a94b
Giorgi Khatiashvili <giorgikhatiashvili93@yahoo.com>
2020-06-08 22:21:26 +0400

    '

---
+++ src/components/Pokemon.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Pokemon({ pokemon }) {
+    return (
+
+        <div className='pokemon'>
+            <div className='pokemon__name'>
+                <p>{pokemon.name}</p>
+            </div>
+            <div className='pokemon__meta'>
+                <p>{pokemon.maxHP}</p>
+                <p>{pokemon.maxCP}</p>
+            </div>
+            <div className='pokemon__image'>
+                <img src={pokemon.image} alt={pokemon.name} />
+            </div>
+            <div className='pokemon__attacks'>
+
+            </div>
+
+
+        </div>
+
+    )
+}
+
+
+export default Pokemon
